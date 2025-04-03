723113ed827363a975f7f7c63c96ba6c1f449453
renan-ag <ag.renan@hotmail.com>
2025-04-03 12:04:22 -0300

    First commit (Project initial structure)

---
+++ src/scripts/app.ts
@@ -0,0 +1,23 @@
+import i18next from "./i18n.ts";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elementsId = ["welcome", "description"];
+  const languageButtons = document.querySelectorAll("button");
+
+  function updateContent(label: string, element: HTMLElement | null) {
+    if (element) element.textContent = i18next.t(label);
+  }
+
+  languageButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const lang = (e.target as HTMLButtonElement).dataset.lang;
+      i18next.changeLanguage(lang);
+    });
+  });
+
+  i18next.on("initialized", () => {
+    elementsId.forEach((label) =>
+      updateContent(label, document.getElementById(label))
+    );
+  });
+});
---
+++ src/scripts/i18n.ts
@@ -0,0 +1,16 @@
+import i18next from "i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18next
+  .use(Backend)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: "pt",
+    supportedLngs: ["pt", "en"],
+    debug: false,
+    interpolation: { escapeValue: false },
+    backend: { loadPath: "/locales/{{lng}}.json" },
+  });
+
+export default i18next;
