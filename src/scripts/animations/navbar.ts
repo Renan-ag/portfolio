47c393640b110b5311a91684ac45ab76b8a6785d
renan-ag <ag.renan@hotmail.com>
2025-04-08 10:35:10 -0300

    Refactoring the code to improve readability, separate mobile and larger screen logic, and fix the mouse event conditional in the setup for larger screens.

--- src/scripts/animations/navbar.ts
+++ src/scripts/animations/navbar.ts
@@ -1,40 +1,104 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-document.addEventListener("DOMContentLoaded", () => {
+const BREAKPOINT_LG = 768;
+const ANIMATION_TYPES = {
+  BOUNCING: "BOUNCING",
+  CLOSE_BTN: "CLOSE_BTN",
+} as const;
+
+const setupMobileMenu = (
+  menuMobile: Element | null,
+  btnMenuMobile: HTMLElement | null,
+  circles: NodeListOf<Element>
+) => {
+  if (!btnMenuMobile || !menuMobile) return;
+
   const tl = gsap.timeline({ paused: true });
+  let currentAnimation: keyof typeof ANIMATION_TYPES = ANIMATION_TYPES.BOUNCING;
 
-  const circles = document.querySelectorAll("[data-target='bx'] > div");
-  const menuMobile = document.querySelector("[data-target='menuMobile']");
-
-  const btnMenuMobile: HTMLElement | null = document.querySelector(
-    "button[data-target='bx']"
-  );
-
-  let currentAnimation: "bouncing" | "closeBtn" = "bouncing";
-
-  circlesBouncing(circles, tl);
+  createBouncingAnimation(circles, tl);
   tl.play();
 
-  btnMenuMobile?.addEventListener("click", () => {
-    menuMobile?.classList.toggle('active');
+  btnMenuMobile.addEventListener("click", () => {
+    menuMobile.classList.toggle("active");
 
-    if (currentAnimation == "bouncing") {
-      resetTl(tl);      
-      transformCirclesInX(circles, tl);
-      currentAnimation = "closeBtn";
-    } else if (currentAnimation == "closeBtn") {
-      tl.reverse();      
+    if (currentAnimation === ANIMATION_TYPES.BOUNCING) {
+      resetTimeline(tl);
+      createCloseButtonAnimation(circles, tl);
+      currentAnimation = ANIMATION_TYPES.CLOSE_BTN;
+    } else {
+      tl.reverse();
       tl.eventCallback("onReverseComplete", () => {
-        resetTl(tl);        
-        circlesBouncing(circles, tl);
+        resetTimeline(tl);
+        createBouncingAnimation(circles, tl);
         tl.play();
-        currentAnimation = "bouncing";
+        currentAnimation = ANIMATION_TYPES.BOUNCING;
       });
     }
   });
-});
+};
 
-function transformCirclesInX(
+const setupDesktopAnimations = (
+  desktopLinks: Element | null,
+  desktopCirclesContainer: Element | null,
+  navElement: Element | null,
+  circles: NodeListOf<Element>
+) => {
+  if (!desktopLinks || !desktopCirclesContainer || !navElement) return;
+  const tlCircles = gsap.timeline({ paused: true });
+  const tlDesktop = gsap.timeline({ paused: true });
+
+  createBouncingAnimation(circles, tlCircles);
+  tlCircles.play();
+
+  tlDesktop.to(desktopLinks, {
+    maxWidth: 0,
+    duration: 0.25,
+    ease: "power1",
+  });
+
+  tlDesktop.to(navElement, { width: "300px", ease: "power1" }, "<");
+  tlDesktop.to(desktopCirclesContainer, {
+    opacity: 1,
+    duration: 0.25,
+    ease: "power1",
+  });
+
+  ScrollTrigger.create({
+    trigger: "main",
+    start: "top top",
+    end: "+=150",
+    onEnter: () => tlDesktop.play(),
+    onLeaveBack: () => tlDesktop.reverse(),
+  });
+
+  const handleMouseEnter = () => {
+    if (window.scrollY > 100) {
+      const mouseEnterTL = gsap.timeline();
+      mouseEnterTL.to(desktopCirclesContainer, { opacity: 0, ease: "power1" });
+      mouseEnterTL.to(desktopLinks, {
+        maxWidth: "400px",
+        duration: 0.25,
+        ease: "power1",
+      });
+      mouseEnterTL.to(navElement, { width: "436px" }, "<");
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (window.scrollY > 100) {
+      gsap.to(desktopLinks, { maxWidth: 0, duration: 0.25, ease: "power1" });
+      gsap.to(navElement, { width: "300px" });
+      gsap.to(desktopCirclesContainer, { opacity: 1, ease: "power1" });
+    }
+  };
+
+  navElement.addEventListener("mouseenter", handleMouseEnter);
+  navElement.addEventListener("mouseleave", handleMouseLeave);
+};
+
+function createCloseButtonAnimation(
   circles: NodeListOf<Element>,
   tl: gsap.core.Timeline
 ) {
@@ -70,8 +134,12 @@ function transformCirclesInX(
   );
 }
 
-function circlesBouncing(circles: NodeListOf<Element>, tl: gsap.core.Timeline) {
+function createBouncingAnimation(
+  circles: NodeListOf<Element>,
+  tl: gsap.core.Timeline
+) {
   tl.delay(1);
+
   circles.forEach((circle) => {
     tl.to(circle, {
       y: -4,
@@ -92,9 +160,34 @@ function circlesBouncing(circles: NodeListOf<Element>, tl: gsap.core.Timeline) {
   tl.repeat(-1);
 }
 
-function resetTl(tl: gsap.core.Timeline) {
+function resetTimeline(tl: gsap.core.Timeline) {
   tl.restart();
   tl.clear();
   tl.repeat(0);
   tl.delay(0);
 }
+
+function init() {
+  const width = window.innerWidth;
+  const isMobile = width < BREAKPOINT_LG;
+
+  if (!isMobile) {
+    setupDesktopAnimations(
+      document.querySelector("[data-target='desktop-navLinksContainer']"),
+      document.querySelector('[data-target="desktop-circlesContainer"]'),
+      document.querySelector("nav"),
+      document.querySelectorAll(".bounceCircle-desktop")
+    );
+  } else {
+    setupMobileMenu(
+      document.querySelector("[data-target='menuMobile']"),
+      document.querySelector("button[data-target='bx']"),
+      document.querySelectorAll(".bounceCircle")
+    );
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+  init();
+});
