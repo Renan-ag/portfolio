4581f0e9eab63725b39f0660fc4620d150ce617e
renan-ag <ag.renan@hotmail.com>
2025-04-13 20:16:49 -0300

    Added all text content in HTML and added all IDs in app.ts to translate content

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,24 +1,66 @@
 import "./animations/animations.ts";
 import i18next from "./i18n.ts";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const elementsId = ["welcome", "description"];
-  const languageButtons = document.querySelectorAll("button");
+const ELEMENTS_ID = [
+  "nav-link-about",
+  "nav-link-projects",
+  "nav-link-skills",
+  "about-title",
+  "about-subtitle",
+  "about-occupation",
+  "about-description",
+  "project-title",
+  "project-spiderman",
+  "project-personal-budget",
+  "project-blog",
+  "skill-title",
+  "experience-title",
+  "experience-1-functions-title",
+  "experience-2-functions-title",
+  "experience-1-occupation",
+  "experience-1-case-1",
+  "experience-1-case-2",
+  "experience-1-case-3",
+  "experience-1-case-4",
+  "experience-2-occupation",
+  "experience-2-case-1",
+  "experience-2-case-2",
+  "experience-2-case-3",
+  "experience-2-case-4",
+  "experience-2-case-5",
+  "experience-2-case-6",
+  "experience-2-case-7",
+  "experience-2-case-8",
+  "experience-2-case-9",
+  "experience-2-case-10",
+  "footer-title",
+  "footer-email",
+  "footer-linkedin",
+  "footer-github",
+];
 
-  function updateContent(label: string, element: HTMLElement | null) {
-    if (element) element.textContent = i18next.t(label);
+document.addEventListener("DOMContentLoaded", () => {
+  const languageButtons = document.querySelectorAll("button[data-lang]");
+  const elementMap = new Map<string, HTMLElement>();
+
+  ELEMENTS_ID.forEach((id) => {
+    const el = document.getElementById(id);
+    if (el) elementMap.set(id, el);
+  });
+
+  function updateAllContent() {
+    elementMap.forEach((el, label) => {
+      el.textContent = i18next.t(label);
+    });
   }
 
   languageButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-      const lang = (e.target as HTMLButtonElement).dataset.lang;
+      const lang = (e.target as HTMLButtonElement).dataset.lang as string;
       i18next.changeLanguage(lang);
     });
   });
 
-  i18next.on("initialized", () => {
-    elementsId.forEach((label) =>
-      updateContent(label, document.getElementById(label))
-    );
-  });
+  i18next.on("initialized", updateAllContent);
+  i18next.on("languageChanged", updateAllContent);
 });
