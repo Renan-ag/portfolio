d06e64466877b98a646daf18579731742f07e148
renan-ag <ag.renan@hotmail.com>
2025-04-13 23:32:21 -0300

    Chaging trigger element ID

--- src/scripts/animations/footer.ts
+++ src/scripts/animations/footer.ts
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(ScrollTrigger);
   const footer = document.querySelector("#scroll");
   const tl = gsap.timeline({ paused: true });
-  tl.from(footer, { translateY: -450 }).to(footer, { translateY: 0 });
+  tl.from(footer, { translateY: -450 }).to(footer, { translateY: 0, ease: "expo"});
 
   ScrollTrigger.create({
-    trigger: "#trigger-footer",
+    trigger: "#Jobs",
     start: "bottom bottom",
-    end: "+=900",
+    end: "+=800",
     scrub: true,
     onUpdate: (self) => {
       tl.progress(self.progress);
